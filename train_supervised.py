5fe27409d955bc0b9406ad2c66877b14d1f37d6c
mwcvitkovic <mwcvitkovic@gmail.com>
2020-11-04 17:15:12 -0800

    Tweak train_supervised.py

--- train_supervised.py
+++ train_supervised.py
@@ -1,20 +1,21 @@
-import torch
-import torch.optim as optim
-import torch.nn as nn
-import torchvision
-import torchvision.transforms as transforms
-import numpy as np
-from tqdm import tqdm
 import json
 import os
+
+import numpy as np
 import replicate
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from tqdm import tqdm
+
 from models import wide_resnet, resnet_official
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 p = os.path.join(dir_path, 'params.json')
 with open(p, 'r') as f:
     params = json.load(f)
-    f.close()
 
 gaussian_noise_var = params["gaussian_noise_var"]
 depth = params["depth"]
@@ -30,6 +31,7 @@ eval_every = params["eval_every"]
 # normalize all pixel values to be in [-1, 1] and add Gaussian noise with mean zero, variance gaussian_noise_var
 transform = transforms.Compose(
     [transforms.ToTensor(),
+     # [0.4914, 0.4822, 0.4465], [0.2488, 0.2453, 0.2633]
      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
      lambda x: x + gaussian_noise_var * torch.randn_like(x)]
 )
@@ -47,36 +49,35 @@ trainloader = torch.utils.data.DataLoader(trainset, batch_size=train_batch_size,
 testloader = torch.utils.data.DataLoader(testset, batch_size=test_batch_size,
                                          shuffle=False, num_workers=2)
 
-#define the device
+# define the device
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # define the model
-#model = wide_resnet.Wide_ResNet(depth=depth, widen_factor=widen_factor, dropout_rate=dropout_rate, num_classes=num_classes)
+# model = wide_resnet.Wide_ResNet(depth=depth, widen_factor=widen_factor, dropout_rate=dropout_rate, num_classes=num_classes)
 model = resnet_official.wrn_28_2().to(device)
 
-#define the optimizer and criterion
+# define the optimizer and criterion
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=learning_rate, betas=[.9, .999])
-
+optimizer = optim.Adam(model.parameters(), lr=learning_rate, betas=(.9, .999))
 
 print("starting training ...")
 
 experiment = replicate.init(
-        path=".",
-        params={"learning_rate": params["learning_rate"], "num_epochs": params["num_epochs"]},
-    )
+    path=".",
+    params={"learning_rate": params["learning_rate"], "num_epochs": params["num_epochs"]},
+)
 
-#create a real training loop
+# create a real training loop
 for epoch in range(num_epochs):
-    for i, data in tqdm(enumerate(trainloader), total=len(trainset) // train_batch_size + 1):
+    for i, (inputs, labels) in tqdm(enumerate(trainloader), total=len(trainset) // train_batch_size + 1):
 
-        #obtain data
-        inputs, labels = data[0].to(device), data[1].to(device)
+        # obtain data
+        inputs, labels = inputs.to(device), labels.to(device)
 
-        #zero gradients
+        # zero gradients
         optimizer.zero_grad()
 
-        #forward, backward, loss
+        # forward, backward, loss
         logits = model(inputs)
         loss = criterion(logits, labels)
         loss.backward()
@@ -85,28 +86,27 @@ for epoch in range(num_epochs):
         if i % 5 == 0:
             tqdm.write(f"loss: {loss.item()}")
 
-
     if epoch % eval_every == 0:
-        #if i % 10 == 0:
-            #total = 0
+        # if i % 10 == 0:
+        # total = 0
         corrects = []
         losses = []
         with torch.no_grad():
             model.eval()
-            #check accuracy on validation set
-            for data in testloader:
-                images, labels = data[0].to(device), data[1].to(device)
-                logits = model(images)
-                loss = nn.CrossEntropyLoss(reduction='none')(logits, labels).cpu().numpy()
+            # check accuracy on validation set
+            for test_inputs, test_labels in testloader:
+                test_inputs, test_labels = test_inputs.to(device), test_labels.to(device)
+                logits = model(test_inputs)
+                loss = nn.CrossEntropyLoss(reduction='none')(logits, test_labels).cpu().numpy()
                 losses.extend(loss)
                 _, predicted = torch.max(logits.data, 1)
-                correct = (predicted == labels).float().cpu().numpy()
+                correct = (predicted == test_labels).float().cpu().numpy()
                 corrects.extend(correct)
             loss = np.mean(losses)
             acc = np.mean(corrects)
             tqdm.write(f"Epoch {epoch} validation accuracy: {acc}, Epoch validation loss: {loss}")
 
-            #save and log a checkpoint
+            # save and log a checkpoint
             torch.save(model, "model.pth")
             experiment.checkpoint(
                 path="model.pth",
