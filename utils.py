4b5b1b37c0c7ab5bad6c10fccbde251a31a633ef
Divyanshu Murli <DivyanshuMurli1@divyanssmacbook.domain>
2020-12-27 19:24:47 -0700

    update doc strings

--- utils.py
+++ utils.py
@@ -1,48 +1,29 @@
 import torch
+import os
 
 
 def create_random_buffer(size, n_channels, im_size):
+
+    """
+    :param size: (int) number of images in buffer
+    :param n_channels: (int) channels
+    :param im_size: (int) image size
+    :return: (arr) buffer
+    """
+
     return torch.FloatTensor(size, n_channels, im_size, im_size).uniform_(-1, 1)
 
 
-def load_model_and_buffer(load_dir, device, with_energy=True):
+def run_sgld(model, x_k, sgld_steps, sgld_step_size, sgld_noise):
 
     """
-    :param load_dir: (str) directory from which to load model and buffer
-    :param with_energy: (bool) if loading an ordinary model, or an energy-trained model with buffer
-    :return: (obj) model (and buffer)
+    :param model: (obj) model
+    :param x_k: (arr) sgld tensor
+    :param sgld_steps: (int) number of sgld steps
+    :param sgld_step_size: (float) sgld step size parameter
+    :param sgld_noise: (float) sgld noise parameter
+    :return:
     """
-
-    if with_energy:
-        print(f"loading model and buffer from {load_dir} ...")
-        model = wide_resnet_energy_output.WRN_Energy(params["depth"], params["widen_factor"], 0.0, 10)
-        checkpoint_dict = torch.load(load_dir)
-        model.load_state_dict(checkpoint_dict["model"])
-        model = model.to(device)
-        buffer = checkpoint_dict["buffer"]
-
-        return model, buffer
-
-    else:
-        print(f"loading model from {load_dir} ...")
-        model = wide_resnet.WideResNet(params["depth"], params["widen_factor"], 0.0, 10)
-        model_dict = torch.load(load_dir)
-        model.load_state_dict(model_dict)
-        model = model.to(device)
-
-        return model
-
-
-def save_checkpoint(save_dir, epoch):
-    print(f"saving model checkpoint at epoch {epoch} ...")
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-    model.cpu()  # TODO: this line doesn't seem to work when training with TPU
-    torch.save(model.state_dict(), f"{save_path_prefix}_{epoch}_epochs.pt")
-    model.to(device)
-    print("checkpoint saved!")
-
-def run_fresh_sgld(model, x_k, sgld_steps, sgld_step_size, sgld_noise):
 
     model.eval()
     for step in range(sgld_steps):
@@ -55,3 +36,47 @@ def run_fresh_sgld(model, x_k, sgld_steps, sgld_step_size, sgld_noise):
     sgld_samples = x_k.detach()
 
     return sgld_samples
+
+
+def load_model_and_buffer(load_dir, model, device, with_energy=True):
+
+    """
+    :param load_dir: (str) directory from which to load model and buffer
+    :param with_energy: (bool) if loading an ordinary model, or an energy-trained model with buffer
+    :return: (obj) model, ((arr) buffer)
+    """
+
+    if with_energy:
+        print(f"loading model and buffer from {load_dir} ...")
+        checkpoint_dict = torch.load(load_dir)
+        model.load_state_dict(checkpoint_dict["model"])
+        model = model.to(device)
+        buffer = checkpoint_dict["buffer"]
+
+        return model, buffer
+
+    else:
+        print(f"loading model from {load_dir} ...")
+        model_dict = torch.load(load_dir)
+        model.load_state_dict(model_dict)
+        model = model.to(device)
+
+        return model
+
+
+def save_checkpoint(model, save_dir, epoch, device):
+
+    """
+    :param model: (obj) model
+    :param save_dir: (str) checkpoint save directory
+    :param epoch: (int) checkpoint epoch
+    :return: None
+    """
+
+    print(f"saving model checkpoint at epoch {epoch} ...")
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    model.cpu()  # TODO: this line doesn't seem to work when training with TPU
+    torch.save(model.state_dict(), f"{save_path_prefix}_{epoch}_epochs.pt")
+    model.to(device)
+    print("checkpoint saved!")
